383dacf8a09be5cfe2d9c1f0dc35b7950bbbeb1c
Styven007 <reyesstiven873@gmail.com>
2025-05-04 00:46:56 -0500

    Primer commit del proyecto Spring Boot

---
+++ src/main/java/com/stiven/miinventario/MiinventarioApplication.java
@@ -0,0 +1,13 @@
+package com.stiven.miinventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiinventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiinventarioApplication.class, args);
+	}
+
+}
